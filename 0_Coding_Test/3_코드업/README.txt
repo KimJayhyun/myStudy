9e6b9f8692adda3aae56ee1c28c71d60917bcf5f
Kimjayhyun_company <dnlwjem000@gmail.com>
2022-02-07 15:50:19 +0900

    update gitignore

--- 0_Coding_Test/3_코드업/README.txt
+++
@@ -1,7 +0,0 @@
-https://codeforces.com/
-
-https://codeup.kr/problem.php?id=
-    문제번호 입력
-
-ID
-    JayhyunKim  
--- 1_visionAI_KOSA/code/myCode/머신러닝/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-X = np.array([i for i in range(10)])
-
-print(X.shape)
-
-y = X.reshape(-1, 1)
-
-print(y.shape)
